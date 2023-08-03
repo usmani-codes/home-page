79aa5971c91c40769eea75f687ecb5a7ead77121
usman <kohistani5525@gmail.com>
2023-08-03 23:59:16 +0500

    complete

--- app.js
+++ app.js
@@ -6,62 +6,23 @@ AOS.init({
   // mirror: false
 })
 
-
 // don't go up
-document.addEventListener('DOMContentLoaded', function () {
-  // Get elements
-  var containerElement = document.querySelector('.wraper')
-  var optionsElement = document.querySelector('.options')
-  var option1Element = document.querySelector('.op-1')
-  var option2Element = document.querySelector('.op-2')
+const icon1 = document.querySelector('.menu-icon')
+const icon2 = document.querySelector('.xmark')
+const xNavEl = document.querySelector('.x-nav')
+const logoEl = document.querySelector('.logo')
+const navEl = document.querySelector('.nav')
 
-  var links1Element = document.querySelector('.links-1')
-  var links2Element = document.querySelector('.links-2')
-
-  // Hover over container to show .options
-  containerElement.addEventListener('mouseenter', function () {
-    optionsElement.style.display = 'flex'
-  })
-
-  containerElement.addEventListener('mouseleave', function () {
-    optionsElement.style.display = 'none'
-    links1Element.style.display = 'none' // Hide .links-1 as well
-  })
-
-  // Hover over .op-1 to show .links-1
-  option1Element.addEventListener('mouseenter', function () {
-    links1Element.style.display = 'block'
-  })
-
-  option1Element.addEventListener('mouseleave', function () {
-    links1Element.style.display = 'none'
-  })
-  // Hover over .op-2 to show .links-2
-  option2Element.addEventListener('mouseenter', function () {
-    links2Element.style.display = 'block'
-  })
-
-  option2Element.addEventListener('mouseleave', function () {
-    links2Element.style.display = 'none'
-  })
+icon1.addEventListener('click', () => {
+  icon1.style.display = 'none'
+  logoEl.style.display = 'none'
+  icon2.style.display = 'block'
+  xNavEl.style.display = 'block'
 })
 
-// document.addEventListener('DOMContentLoaded', function () {
-//   const showMoreLink = document.querySelector('.show-more')
-//   const optionsWrapper = document.querySelector('.options-wraper')
-//   const options = document.querySelector('.options')
-
-//   showMoreLink.addEventListener('mouseenter', function () {
-//     options.style.display = 'flex'
-//   })
-
-//   optionsWrapper.addEventListener('mouseleave', function () {
-//     options.style.display = 'none'
-//   })
-
-//   document.addEventListener('mouseleave', function (event) {
-//     if (!showMoreLink.contains(event.relatedTarget)) {
-//       options.style.display = 'none'
-//     }
-//   })
-// })
+icon2.addEventListener('click', () => {
+  logoEl.style.display = 'initial'
+  icon2.style.display = 'none'
+  icon1.style.display = 'block'
+  xNavEl.style.display = 'none'
+})
