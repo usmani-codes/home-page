011b8f20c8b55ccb6ff245dccfb24eb80c30ea15
usman <kohistani5525@gmail.com>
2023-08-04 20:39:11 +0500

    menu-icon bug fix

--- app.js
+++ app.js
@@ -11,7 +11,17 @@ const icon1 = document.querySelector('.menu-icon')
 const icon2 = document.querySelector('.xmark')
 const xNavEl = document.querySelector('.x-nav')
 const logoEl = document.querySelector('.logo')
-const navEl = document.querySelector('.nav')
+// const navEl = document.querySelector('.nav')
+
+const closeIcons = () => {
+  icon1.style.display = 'none'
+  icon2.style.display = 'none'
+  xNavEl.style.display = 'none'
+  logoEl.style.display = 'initial'
+}
+const openIcon1 = () => {
+  icon1.style.display = 'block'
+}
 
 icon1.addEventListener('click', () => {
   icon1.style.display = 'none'
@@ -26,3 +36,25 @@ icon2.addEventListener('click', () => {
   icon1.style.display = 'block'
   xNavEl.style.display = 'none'
 })
+
+function breakpointManager() {
+  const breakpointWidth = 1024
+  let isAboveBreakpoint = window.innerWidth >= breakpointWidth
+
+  function handleResize() {
+    const newIsAboveBreakpoint = window.innerWidth >= breakpointWidth
+    if (newIsAboveBreakpoint !== isAboveBreakpoint) {
+      isAboveBreakpoint = newIsAboveBreakpoint
+      triggerCallback()
+    }
+  }
+
+  function triggerCallback() {
+    isAboveBreakpoint ? closeIcons() : openIcon1()
+  }
+
+  window.addEventListener('resize', handleResize)
+  triggerCallback()
+}
+
+breakpointManager()
